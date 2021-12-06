250f7eb30cba5e30e2b05314ece85c9b09964c3e
Matt Boutell <boutell@gmail.com>
2021-12-06 13:23:45 -0500

    Add compose email and alert dialog.

--- ActivityLifecycleDemo/app/src/main/java/edu/rosehulman/activitylifecycledemo/MainActivity.kt
+++ ActivityLifecycleDemo/app/src/main/java/edu/rosehulman/activitylifecycledemo/MainActivity.kt
@@ -1,9 +1,11 @@
 package edu.rosehulman.activitylifecycledemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import edu.rosehulman.activitylifecycledemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +13,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(Constants.TAG, "onCreate() called")
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupButtons()
@@ -24,14 +27,32 @@ class MainActivity : AppCompatActivity() {
 
         binding.buttonDialog.setOnClickListener {
             Log.d(Constants.TAG, "Clicked dialog button")
-            // TODO: make dialog
+            val dialog = AlertDialog.Builder(this)
+                .setTitle("I'm an AlertDialog!")
+                .setPositiveButton(android.R.string.ok) { dialog, which -> Log.d(Constants.TAG, "Dialog OK button pressed") }
+                .show()
+//                dialog.setPositiveButton(android.R.string.ok) {dialog, which -> Log.d(Constants.TAG, "Dialog OK button pressed")}
         }
 
         binding.fab.setOnClickListener {
             Log.d(Constants.TAG, "Clicked FAB = floating action button")
-            // TODO: launch intent
+            // From https://developer.android.com/guide/components/intents-common#ComposeEmail
+            composeEmail(arrayOf("boutell@gmail.com"), "Demo app")
         }
     }
+
+    private fun composeEmail(addresses: Array<String>, subject: String) {
+        val intent = Intent(Intent.ACTION_SEND).apply {
+            type = "*/*"
+            putExtra(Intent.EXTRA_EMAIL, addresses)
+            putExtra(Intent.EXTRA_SUBJECT, subject)
+            putExtra(Intent.EXTRA_TEXT, "Your demo app mailed you this - it's not spam!")
+        }
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+    }
+
 
     override fun onStart() {
         super.onStart()
